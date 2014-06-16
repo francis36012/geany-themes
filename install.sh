1bc45345a37d5dba5a68f91e574468c7752d7d00
Francis Agyapong <francisagyapong2@gmail.com>
2014-06-15 19:33:34 -0600

    add system-wide installation option in installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
-CSDIR="$HOME/.config/geany/colorschemes/"
-echo "Installing themes into \`$CSDIR'..."
-mkdir -p "$CSDIR"
-for SCHEME in `ls colorschemes/*.conf`
-do
-  BNAME=`basename "$SCHEME"`
-  echo " => $BNAME"
-  cp "$SCHEME" "$CSDIR"
-done
+#!/bin/bash
+USRCSDIR="$HOME/.config/geany/colorschemes/"
+SYSCSDIR="/usr/share/geany/colorschemes/"
+
+if [ $EUID -ne 0 ] ; then
+
+	echo "Installing themes into $USRCSDIR..."
+	mkdir -p "$USRCSDIR"
+	for SCHEME in $(ls colorschemes/*.conf)
+	do
+		BNAME=$(basename $SCHEME)
+		echo " => $BNAME"
+		cp $SCHEME $USRCSDIR
+	done
+
+else
+	echo "Installing themes into $SYSCSDIR..."
+	for SCHEME in $(ls colorschemes/*.conf)
+	do
+		BNAME=$(basename $SCHEME)
+		echo " => $BNAME"
+		cp $SCHEME $SYSCSDIR
+	done
+fi
